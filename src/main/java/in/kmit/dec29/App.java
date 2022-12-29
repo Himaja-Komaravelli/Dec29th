812493dbb8b73da343f7c8db0e6367f722849563
Himaja-Komaravelli <20bd1a1231@gmail.com>
2022-12-29 19:01:47 +0530

    trying to push

---
+++ src/main/java/in/kmit/dec29/App.java
@@ -0,0 +1,13 @@
+package in.kmit.dec29;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "2022 gonna end soon!" );
+    }
+}
